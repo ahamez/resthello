5c0a24005ce1f6976af2bbec59aa338f21a607dc
Alexandre Hamez <alexandre.hamez@gmail.com>
2015-02-22 16:04:45 +0100

    Try motor

--- tornado/app.py
+++ tornado/app.py
@@ -1,28 +1,82 @@
-#! /usr/bin/env python
+#! /usr/bin/env python3
+
+import concurrent.futures
 
 import tornado.ioloop
 import tornado.web
 import tornado.options
-import sys
+import tornado.gen
+import tornado.httpclient
+import motor
 
-class RootHandler(tornado.web.RequestHandler):
-  def get(self):
-    self.write("Hello, world")
+import mongoengine
 
-class FooHandler(tornado.web.RequestHandler):
-  def get(self):
-    self.write("Foo")
 
-application = tornado.web.Application([
-  (r"/", RootHandler),
-  (r"/foo", FooHandler)
-])
+class Foo(mongoengine.Document):
+    field1 = mongoengine.StringField(required=True)
+    field2 = mongoengine.StringField(max_length=50)
 
-if __name__ == "__main__":
 
-  # args = sys.argv
-  # args.append("--log_file_prefix=/Users/hal/Desktop/my_app.log")
-  # tornado.options.parse_command_line(args)
+class Simple(tornado.web.RequestHandler):
+    @tornado.gen.coroutine
+    def get(self):
+        query_res = yield tornado.gen.Task(Simple.do_query)
+        self.write("Simple" + query_res)
+        # self.write({'Foo':'simple'})
+        self.finish()
 
-  application.listen(8080)
-  tornado.ioloop.IOLoop.instance().start()
+    @staticmethod
+    def do_query(callback):
+        query = Foo.objects(field1="1")
+        query.no_cache()
+        res = "".join(foo.field1 for foo in query)
+        return callback(res)
+
+
+class Pool(tornado.web.RequestHandler):
+    @tornado.gen.coroutine
+    def get(self):
+        query_res = yield self.settings['pool'].submit(Pool.do_query)
+        self.write("Pool" + query_res)
+
+    @staticmethod
+    def do_query():
+        query = Foo.objects(field1="1")
+        query.no_cache()
+        res = "".join(foo.field1 for foo in query)
+        return res
+
+
+class Motor(tornado.web.RequestHandler):
+    @tornado.gen.coroutine
+    def get(self):
+        db = self.settings['db']
+        user = yield db.users.find_one({'_id': 'saucisson'})
+        yield db.billings.update({'_id': 'saucisson'}, {'key': 'value'})
+        projet = yield db.projects.find_one({'_id': 'cosyverif'})
+        yield db.results.update({'_id': 'cosyverif'}, {'key': 'value'})
+        locale = yield db.locales.find_one({'_id': 'en'})
+        self.write('foo' + str(user) + str(projet) + str(locale))
+
+
+def main():
+    tornado.options.parse_command_line()
+
+    pool = concurrent.futures.ThreadPoolExecutor(max_workers=5)
+    db = motor.MotorClient().paquito
+    users = db['users']
+
+    mongoengine.connect('localhost')
+
+    application = tornado.web.Application([(r"/simple", Simple),
+                                           (r"/pool", Pool),
+                                           (r"/motor", Motor)
+                                           ], db=db, users=users, pool=pool, debug=True
+    )
+
+    application.listen(8080)
+    tornado.ioloop.IOLoop.instance().start()
+
+
+if __name__ == '__main__':
+    main()
